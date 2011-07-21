b3a97abcc9a8abea4e786c03c638b4e907a49094
Dave Newman <dave@snappyco.de>
2011-07-21 16:55:23 +1000

    Revert "worlds can now be communicated with through world.stdin"
    This reverts commit 65db19e8a5dc7b2a533bdf14b81e230bd634abc3.

--- lib/minefold/worker.rb
+++ lib/minefold/worker.rb
@@ -75,9 +75,7 @@ class Worker
       "cd ~/minefold",
       "GIT_SSH=~/deploy-ssh-wrapper git pull origin master",
       "bundle install --without proxy development test cli",
-      "sudo god",
-      "sudo god load ~/minefold/worker/config/worker.god",
-      "sudo god start worker-app",
+      "sudo god -c ~/minefold/worker/config/worker.god",
     ]
 
     results = server.ssh commands.join(" && ")
