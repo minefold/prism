fe255d2696a51e148bc718ae64aeab1f756b563e
Dave Newman <dave@snappyco.de>
2011-07-25 20:54:54 +1000

    Revert "Revert "moved environments into dev and prod""
    This reverts commit cd3c7d37f4adae938dbcb0368d38435585fe7433.

---
+++ config/development.rb
@@ -0,0 +1,11 @@
+ROOT = File.expand_path "../..", __FILE__
+BIN = "#{ROOT}/bin"
+LIB = "#{ROOT}/lib"
+WORLDS = "#{ROOT}/worlds"
+LOG_PATH = "#{ROOT}/log"
+
+JAR = "#{WORLDS}/server.jar"
+PIDS = "#{ROOT}/tmp/pids"
+
+MONGOHQ_URL='mongodb://localhost'
+REDISTOGO_URL="redis://localhost/"
