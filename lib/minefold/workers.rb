59bc27d40362db0167528217be352d357c4aa60d
Dave Newman <dave@snappyco.de>
2011-07-28 17:09:01 +1000

    hard coding workers as production in master branch

--- lib/minefold/workers.rb
+++ lib/minefold/workers.rb
@@ -13,7 +13,7 @@ class Workers
   
   def self.existing
     compute_cloud.servers.
-      select {|s| s.tags["Name"] != "Proxy" && s.tags["environment"] != "staging" }.
+      select {|s| s.tags["Name"] != "worker" && s.tags["environment"] == "production" }.
       map{|s| Worker.new s }
   end
 
