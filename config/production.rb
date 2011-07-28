7ccf42bd46da31f62129adda403b62cb640b004c
Dave Newman <dave@snappyco.de>
2011-07-28 16:54:39 +1000

    new production keys

--- config/production.rb
+++ config/production.rb
@@ -10,8 +10,8 @@ PIDS = "#{ROOT}/tmp/pids"
 # TODO: put these somewhere more secure
 EC2_SECRET_KEY="4VI8OqUBN6LSDP6cAWXUo0FM1L/uURRGIGyQCxvq"
 EC2_ACCESS_KEY="AKIAJPN5IJVEBB2QE35A"
-MONGOHQ_URL="mongodb://heroku:os304dfe5d3tbkn3r1ko8j@staff.mongohq.com:10002/app578527"
-REDISTOGO_URL="redis://redistogo:9b5f916f86dd29f26ca4af54c3f8f768@catfish.redistogo.com:9527/"
+MONGOHQ_URL="mongodb://heroku:15a8f355615361aeda458c6d887aada2@staff.mongohq.com:10023/app650862"
+REDISTOGO_URL="redis://redistogo:0128df27dcecc0dac569b231d5bd7ccb@angler.redistogo.com:9097/"
 
 SSH_PRIVATE_KEY_PATH="#{ROOT}/.ssh/minefold.pem"
 
