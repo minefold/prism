f45431bd8d8f0303215a3fe694e1e5ca9134f6c5
Dave Newman <dave@snappyco.de>
2011-07-31 11:37:15 +1000

    sudo rm the folder away if its there

--- lib/minefold/worker/cloud.rb
+++ lib/minefold/worker/cloud.rb
@@ -68,7 +68,7 @@ module Worker
       return [] unless public_ip_address
  
       begin
-        server_info = JSON.paxrse get("/worlds", timeout:20).body
+        server_info = JSON.parse get("/worlds", timeout:20).body
         Worlds.new self, server_info.map {|h| World.new self, h["id"], h["port"]}
       rescue => e
         puts e.inspect
@@ -112,7 +112,7 @@ module Worker
       ensure_god_isnt_running = "#{god} quit"
       ensure_thin_isnt_running = "ps -eF | grep 'thin' | awk '{print $2}' | sudo xargs kill -9"
       write_out_env_vars = "echo #{Fold.env} > ~/FOLD_ENV && echo #{Fold.worker_user} > ~/FOLD_WORKER_USER"
-      clone_repo = "cd ~ && rm -rf minefold && GIT_SSH=~/deploy-ssh-wrapper git clone -q --depth 1 -b #{Fold.worker_git_branch} #{WORKER_GIT_REPO}"
+      clone_repo = "cd ~ && sudo rm -rf minefold && GIT_SSH=~/deploy-ssh-wrapper git clone -q --depth 1 -b #{Fold.worker_git_branch} #{WORKER_GIT_REPO}"
       bundle_install = "cd ~/minefold && sudo bundle install --quiet --without proxy development test cli"
       start_god = "cd ~/minefold && #{god} -c ~/minefold/worker/config/worker.god"
       
