212abb53577fb126102c2106aded662e0b3ac6fd
Dave Newman <dave@snappyco.de>
2011-08-25 23:41:57 +1000

    PROXY > subscriptions should be cancelled when player disconnects

--- lib/prism/front/known_player_handler.rb
+++ lib/prism/front/known_player_handler.rb
@@ -14,11 +14,12 @@ module Prism
     
     def exit
       stop_keepalive
+      @subscription.unsubscribe "players:connection_request"
     end
     
     def request_player_connection
       PrismRedis.new do |redis|
-        redis.rpc_json "players:connection_request", username do |connection|
+        @subscription = redis.rpc_json "players:connection_request", username do |connection|
           new_handler ConnectedPlayerHandler, username, connection["host"], connection["port"] if connection_active
         end
       end
