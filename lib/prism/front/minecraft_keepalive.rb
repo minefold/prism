9204e1bd3bb472830cdc11aa92a705336a9219df
Dave Newman <dave@snappyco.de>
2011-09-15 23:34:38 +1000

    setting keepalive back to 15 secs

--- lib/prism/front/minecraft_keepalive.rb
+++ lib/prism/front/minecraft_keepalive.rb
@@ -3,7 +3,7 @@ module Prism
     extend Debugger
     
     def start_keepalive
-      @keepalive = EM::PeriodicTimer.new 3 do
+      @keepalive = EM::PeriodicTimer.new 15 do
         debug "ping"
         connection.send_data MinecraftPackets.create_server 0x00, :keepalive_id => 1337
       end
