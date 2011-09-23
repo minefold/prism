7cefdda5ae9b8c3c0890afe272a89ea24138e852
Dave Newman <dave@snappyco.de>
2011-09-23 20:23:49 +1000

    redis task

--- lib/tasks/redis_tasks.rb
+++ lib/tasks/redis_tasks.rb
@@ -13,16 +13,27 @@ namespace "redis" do
   task "state" do
     redis = redis_connect
     puts "clients: #{redis.info['connected_clients']}"
-    ["players:playing", "usernames",
-     "prism:active_connections",
-     "worlds:running", "worlds:busy",
-     "workers:running", "workers:busy"].each do |hash|
-       puts hash
-       p redis.hgetall(hash)
-       puts
+    %W[players:playing 
+       usernames 
+       prism:active_connections
+       worlds:running
+       worlds:busy
+       workers:running
+       workers:busy
+    ].each do |hash|
+      puts hash
+      p redis.hgetall(hash)
+      puts
     end
-
-    ["players:minute_played", "players:requesting_connection", "players:disconnection_request"].each do |list|
+    
+    %W[players:minute_played 
+       players:requesting_connection 
+       players:disconnection_request
+       workers:requests:create
+       workers:requests:fix
+       workers:requests:start
+       workers:requests:stop
+      ].each do |list|
       length = redis.llen list
       puts list
       p redis.lrange(list, 0, length)
