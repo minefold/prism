a625b811d4847ad7ddee235084d461f32cc9a110
Dave Newman <dave@snappyco.de>
2011-10-06 11:16:23 +1100

    adding seconds to ping time

--- lib/prism/front/minecraft_keepalive.rb
+++ lib/prism/front/minecraft_keepalive.rb
@@ -5,8 +5,9 @@ module Prism
     extend Debugger
     
     def start_keepalive
+      started_at = Time.now
       @keepalive = EM::PeriodicTimer.new 15 do
-        debug "ping"
+        debug "ping - #{(Time.now - started_at)} seconds"
         connection.send_data server_packet 0x00, :keepalive_id => 1337
       end
     end
