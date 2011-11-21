95e48961fe516bc175721bf531c3e4d2be5b6ec7
Chris Lloyd <christopher.lloyd@gmail.com>
2011-11-21 11:15:44 -0800

    Make "unlimited" players actually be unlimited (and remove references to the Pro plan).

--- lib/prism/back/events/player_minute_played_event.rb
+++ lib/prism/back/events/player_minute_played_event.rb
@@ -3,30 +3,30 @@ module Prism
     include ChatMessaging
 
     process "players:minute_played", :username, :timestamp
-    
+
     def log_tag; username; end
 
     def run
       op = redis.hget "usernames", username
       op.callback do |user_id|
-        EM.defer(proc { 
-            mongo_connect.collection('users').find_one({"_id"  => BSON::ObjectId(user_id) }) 
-          }, proc { |user| 
+        EM.defer(proc {
+            mongo_connect.collection('users').find_one({"_id"  => BSON::ObjectId(user_id) })
+          }, proc { |user|
             EM.defer(proc {
-              update = { 
+              update = {
                 '$push' => {'credit_events' => { 'created_at' => Time.now.utc, 'delta' => -1 } },
                 '$inc'  => {'minutes_played' => 1 }
               }
 
-              unless user['plan'] && user['plan'] == 'pro'
-                update.merge!({'$inc'  => {'credits' => -1, 'minutes_played' => 1 }})
+              unless user['unlimited']
+                update.merge! '$inc'  => {'credits' => -1}
               end
-              
+
               mongo_connect.collection('users').find_and_modify({ query: {"_id"  => BSON::ObjectId(user_id) }, update:update })
             }, proc { |user|
               credits = user['credits']
-              if user['plan'] && user['plan'] == 'pro'
-                info "played:#{user['minutes_played']} minutes [PRO ACCOUNT]"
+              if user['unlimited']
+                info "played:#{user['minutes_played']} minutes [UNLIMITED]"
               else
                 info "deducting 1 credit. #{credits} remaining. Played:#{user['minutes_played']} minutes"
                 credits_updated user_id, credits
@@ -41,13 +41,13 @@ module Prism
         15 => "15 minefold minutes left",
         5  =>  "5 minefold minutes left!"
       }.freeze
-      
+
       if (message = messages[credits_remaining]) || credits_remaining < 1
         op = redis.hget "players:playing", username
         op.callback do |world_id|
           redis.hget_json "worlds:running", world_id do |world|
             send_world_player_message world['instance_id'], world_id, username, message if message
-            
+
             if credits_remaining < 1
               EM.add_timer(1)  { send_world_player_message world['instance_id'], world_id, username, "Top up your account at minefold.com" }
               EM.add_timer(40) { send_world_player_message world['instance_id'], world_id, username, "Thanks for playing!" }
--- lib/prism/back/requests/player_connection_request.rb
+++ lib/prism/back/requests/player_connection_request.rb
@@ -1,19 +1,19 @@
 module Prism
   class PlayerConnectionRequest < Request
     include Mixpanel::EventTracker
-    
+
     process "players:connection_request", :username, :remote_ip
 
     def log_tag; username; end
-    
+
     def run
       debug "processing #{username}"
 
       EM.defer(proc { mongo_connect.collection('users').find_one(:safe_username => username.downcase) }, proc { |user|
         if user
           @mp_id, @mp_name = user['_id'].to_s, user['safe_username']
-          
-          if (user['plan'] && user['plan'] == 'pro') || user['credits'] > 0
+
+          if user['unlimited'] || user['credits'] > 0
             recognised_player_connecting user
           else
             mixpanel_track 'bounced'
@@ -23,46 +23,46 @@ module Prism
           mixpanel_track 'rejected'
           unrecognised_player_connecting
         end
-      }) 
+      })
     end
-    
+
     def recognised_player_connecting user
       user_id, world_id = "#{user['_id']}", "#{user['current_world_id']}"
       debug "found user:#{user_id} world:#{world_id}"
-      
+
       if world_id && world_id.size > 0
         redis.hset "usernames", username, user_id
         redis.hset "players:playing", username, world_id
         redis.lpush_hash "players:world_request", username:username, user_id:user_id, world_id:world_id, credits:user['credits']
-        
+
         record_connection user_id, world_id
       else
         info "user:#{username} has no world"
         redis.publish_json "players:connection_request:#{username}", rejected:'no_world'
       end
     end
-    
+
     def unrecognised_player_connecting
       info "unrecognised"
       redis.publish_json "players:connection_request:#{username}", rejected:'unrecognised_player'
     end
-    
+
     def no_credit_player_connecting
       info "user:#{username} has no credit"
       redis.publish_json "players:connection_request:#{username}", rejected:'no_credit'
     end
-    
+
     def record_connection user_id, world_id
       EM.defer(proc {
-          mongo_connect.collection('users').find_and_modify({ 
+          mongo_connect.collection('users').find_and_modify({
             query: {
-              "_id"  => BSON::ObjectId(user_id) 
-            }, update: { 
+              "_id"  => BSON::ObjectId(user_id)
+            }, update: {
               '$set' => {'last_connected_at' => Time.now.utc }
-            } 
+            }
           })
         })
     end
-    
+
   end
 end
