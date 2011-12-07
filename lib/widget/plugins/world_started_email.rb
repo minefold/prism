e4bfff5ac0e90d1ed7bc43d25854f84f82d2db67
Dave Newman <dave@snappyco.de>
2011-12-07 15:35:00 +1100

    send an email 3 mins after world starts

--- lib/widget/plugins/world_started_email.rb
+++ lib/widget/plugins/world_started_email.rb
@@ -1,17 +1,14 @@
 module Widget
   class WorldStartedEmail < WorldPlugin
-
     def world_started
-      puts "PLUGIN: world_started"
-      # @timer = EM.add_periodic_timer(3 * 60) do
-      #   @timer.cancel
-      #   Resque.push 'mailer', class: 'WorldMailer', args: ['world_started', world_id]
-      # end
+      @timer = EM.add_periodic_timer(3 * 60) do
+        @timer.cancel
+        Resque.push 'mailer', class: 'WorldMailer', args: ['world_started', world_id]
+      end
     end
     
     def world_stopped
-      puts "PLUGIN: world_stopped"
-      # @timer.cancel if @timer
+      @timer.cancel if @timer
     end
   end
 end
