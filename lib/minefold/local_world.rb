8026280b9e71d4854ef699bb1dff8d08013c8196
Dave Newman <dave@snappyco.de>
2012-01-04 23:06:19 -0800

    Back to online-mode

--- lib/minefold/local_world.rb
+++ lib/minefold/local_world.rb
@@ -11,7 +11,7 @@ class LocalWorld
         "level-name"       => world['_id'].to_s,
         "level-seed"       => world['seed'].to_s,
         "max-players"      => 1000,
-        "online-mode"      => false,
+        "online-mode"      => true,
         "difficulty"       => world['difficulty'].to_s,
         "gamemode"         => world['game_mode'].to_s,
         "pvp"              => (world['pvp'] || false).to_s,
