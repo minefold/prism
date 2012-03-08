1d8d04c99d174372c5036da7352448bb8d7b39c7
Dave Newman <dave@snappyco.de>
2012-03-08 13:55:00 -0800

    Don't crash on bad connection packet

--- lib/prism/front/unknown_connection_handler.rb
+++ lib/prism/front/unknown_connection_handler.rb
@@ -8,12 +8,19 @@ module Prism
 
       header = data.unpack('C').first
       if header == 0x02
-        username = data[3..-1].force_encoding('UTF-16BE').encode('UTF-8')
-        target_host = nil
-        if username.include?(';')
-          username, target_host = username.split(';', 2)
+        raw_user_data = data[3..-1]
+        if raw_user_data
+          username = raw_user_data.force_encoding('UTF-16BE').encode('UTF-8')
+          target_host = nil
+          if username.include?(';')
+            username, target_host = username.split(';', 2)
+          end
+          new_handler KnownPlayerHandler, username, target_host
+        else
+          connection.send_data server_packet 0xFF, :reason => "Protocol Error"
+          connection.close_connection_after_writing
+          Exceptional.rescue { raise "Invalid connection packet: #{data}" }
         end
-        new_handler KnownPlayerHandler, username, target_host
       elsif header == 0xFE
         connection.send_data server_packet 0xFF, :reason => "Change worlds at minefold.com"
         connection.close_connection_after_writing
