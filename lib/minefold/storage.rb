ddd102cb4580e9b79020262bff68c08a7c456726
Dave Newman <dave@snappyco.de>
2012-04-02 15:17:13 -0700

    retry

--- lib/minefold/storage.rb
+++ lib/minefold/storage.rb
@@ -8,9 +8,10 @@ class Storage
     begin
       new provider.directories.create(:key => OLD_WORLDS_BUCKET, :public => false)
     rescue => e
-      puts "failed #{retry_times} time: #{e}\n#{e.backtrace.join("\n")}"
+      puts "failed #{retry_times} times: #{e}\n#{e.backtrace.join("\n")}"
       sleep retry_timeout
       retry_timeout += 1
+      retry_times += 1
       retry if (retry_times < 10)
       raise e
     end
@@ -21,9 +22,10 @@ class Storage
     begin
       new provider.directories.create(:key => WORLDS_BUCKET, :public => false)
     rescue => e
-      puts "failed #{retry_times} time: #{e}\n#{e.backtrace.join("\n")}"
+      puts "failed #{retry_times} times: #{e}\n#{e.backtrace.join("\n")}"
       sleep retry_timeout
       retry_timeout += 1
+      retry_times += 1
       retry if (retry_times < 10)
       raise e
     end
