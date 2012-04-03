8ee5588176f6d78d76032b1ad6e8b5fa8be53425
Dave Newman <dave@snappyco.de>
2012-04-03 10:28:40 -0700

    fix

--- lib/prism/back/requests/player_connection_request.rb
+++ lib/prism/back/requests/player_connection_request.rb
@@ -165,14 +165,16 @@ module Prism
     # TODO: remove this old school path
     def connect_to_world player, world
       if world
-        if world.has_data_file? # TODO: Make this async!
-          debug "world:#{world.id} found"
-          connect_unvalidated_player_to_current_world player, world
-        else
-          error "world:#{world.id} data_file:#{world.world_data_file} does not exist"
-          redis.publish_json "players:connection_request:#{username}",
-            rejected:'500',
-            details:"world data #{world.world_data_file} not found"
+        world.has_data_file? do |has_data_file|
+          if has_data_file
+            debug "world:#{world.id} found"
+            connect_unvalidated_player_to_current_world player, world
+          else
+            error "world:#{world.id} data_file:#{world.world_data_file} does not exist"
+            redis.publish_json "players:connection_request:#{username}",
+              rejected:'500',
+              details:"world data #{world.world_data_file} not found"
+          end
         end
       else
         debug "world not found"
