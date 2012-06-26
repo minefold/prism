bcecb4549b6e73a756dde3cfc956d908911a8f1e
Dave Newman <dave@snappyco.de>
2012-06-25 17:51:38 -0700

    ok, replica sets actually work now

--- lib/prism/prism_mongo.rb
+++ lib/prism/prism_mongo.rb
@@ -4,9 +4,21 @@ module Prism
 
     def mongo_connect
       @connection ||= begin
-        mongo = ::Mongo::Connection.from_uri(ENV['MONGOHQ_URL'] || MONGOHQ_URL)
-        db_name = mongo.auths.any? ? mongo.auths.first['db_name'] : 'minefold'
-        mongo[db_name]
+        uri = ENV['MONGOHQ_URL'] || MONGOHQ_URL
+        mongo = ::Mongo::Connection.from_uri(uri)
+
+        if mongo.is_a? ::Mongo::ReplSetConnection
+          # this should be in the damn ruby driver
+          mongo_uri = ::Mongo::URIParser.new(uri)
+          auth = mongo_uri.auths.first
+
+          db = mongo[auth['db_name']]
+          db.authenticate auth['username'], auth['password']
+          db
+        else
+          db_name = mongo.auths.any? ? mongo.auths.first['db_name'] : 'minefold'
+          mongo[db_name]
+        end
       end
     end
   end
